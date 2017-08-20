6ff465a0128b4565941927079923a873aa1c30f7
Ines <ihuertas@datatons.com>
2017-08-20 20:00:29 +0200

    update

--- javascripts/client.js
+++ javascripts/client.js
@@ -4,9 +4,9 @@ $.ajax({
   success: function(data) {
     //$("pre").text(JSON.stringify(data, null, "\t"));
 
-    var tableData = '<table><tr><td>Category</td><td>cvcm</td><td>id</td><td>material_usage</td><td>mfr</td><td>SCH</td> </tr>';
+    var tableData = '<table><tr><td>Category</td><td>cvcm</td><td>material_usage</td><td>mfr</td></tr>';
     $.each(data, function(index, data) {
-    tableData += '<tr><td>'+data.category+'</td> <td>'+data.cvcm+'</td> <td>'+data.id+'</td> <td>'+data.material_usage+'</td> <td>'+data.mfr+'</td> <td>'+data.SCH+'</td></tr>';
+    tableData += '<tr><td>'+data.category+'</td> <td>'+data.cvcm+'</td> <td>'+data.material_usage+'</td> <td>'+data.mfr+'</td></tr>';
     });
 
 $('pre').html(tableData);
