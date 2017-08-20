24a7f8e51b226e87c37a87523ef72814fc1d0ac8
Ines <ihuertas@datatons.com>
2017-08-20 19:55:02 +0200

    update info into table

--- javascripts/client.js
+++ javascripts/client.js
@@ -1,20 +1,20 @@
 $.ajax({
-//  url: "https://data.nasa.gov/resource/gymh-eyc2.json",
- url: "https://data.nasa.gov/resource/gymh-eyc2.json?$limit=2&$offset=2",
- data: {},
+  url: "https://data.nasa.gov/resource/gymh-eyc2.json",
+  data: {},
   success: function(data) {
-  //$("pre").text(JSON.stringify(data, null, "\t"));
+    //$("pre").text(JSON.stringify(data, null, "\t"));
+
+    var tableData = '<table><tr><td>Category</td><td>cvcm</td><td>id</td><td>material_usage</td><td>mfr</td><td>SCH</td> </tr>';
+    $.each(data, function(index, data) {
+    tableData += '<tr><td>'+data.category+'</td> <td>'+data.cvcm+'</td> <td>'+data.id+'</td> <td>'+data.material_usage+'</td> <td>'+data.mfr+'</td> <td>'+data.SCH+'</td></tr>';
+    });
+
+$('pre').html(tableData);
 
 
 
-  var tableData = '<table><tr><td>Category</td><td>Id</td></tr>';
-  $.each(text(JSON.stringify(data, null, "\t")), function(index, data) {
-  tableData += '<tr><td>'+data.category+'</td><td>'+data.id'</td></tr>';
-});
-
-$('div').html(tableData);
+  },
   error: function(msg) {
     //todo: handle error gracefully
   }
 });
-
