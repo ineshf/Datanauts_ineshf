1c8bb453e8359736bcc4e906455f6428eabd29b3
Ines <ihuertas@datatons.com>
2017-08-20 19:27:12 +0200

    update

--- javascripts/client.js
+++ javascripts/client.js
@@ -3,11 +3,16 @@ $.ajax({
  url: "https://data.nasa.gov/resource/gymh-eyc2.json?$limit=2&$offset=2",
  data: {},
   success: function(data) {
-   // $("pre").text(JSON.stringify(data, null, "\t"));
-   for(i=0;i<3;i++){
-     $('#here_table').append(text(JSON.stringify(data, null, "\t")) +  i );
-   }
-  },
+  //$("pre").text(JSON.stringify(data, null, "\t"));
+
+
+
+  var tableData = '<table><tr><td>Category</td><td>Id</td></tr>';
+  $.each(text(JSON.stringify(data, null, "\t")), function(index, data) {
+  tableData += '<tr><td>'+data.category+'</td><td>'+data.id'</td></tr>';
+});
+
+$('div').html(tableData);
   error: function(msg) {
     //todo: handle error gracefully
   }
